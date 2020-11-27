0200fcd76549040bc266fe02d8caff6d2242a496
Random936 <randomdude936@gmail.com>
2020-11-27 15:08:36 -0800

    Update immutablestrings.h

--- immutablestrings.h
+++ immutablestrings.h
@@ -4,6 +4,58 @@
 
 /*
 --------------------------------------------------
+    Easy Linked Lists
+--------------------------------------------------
+*/
+
+#define declareLinkedList(type, list_s, list, list_node_s, list_node) \
+typedef struct list_node_s { \
+    type value; \
+    struct list_node_s *next; \
+} list_node; \
+ \
+typedef struct list_s { \
+    void (*createNode) (struct list_s *self, type data); \
+    void (*print) (struct list_s *self, char *formatter); \
+    int valuesize; \
+    struct list_node_s *head; \
+} list; \
+ \
+void list_createNode(list *self, type data) { \
+ \
+    self->valuesize++; \
+    list_node *node = malloc(sizeof(list_node)); \
+    node->value = data; \
+ \
+    if (self->head == NULL) { \
+        node->next = NULL; \
+        self->head = node; \
+    } else { \
+        node->next = self->head; \
+        self->head = node; \
+    } \
+} \
+ \
+void list_print(list *self, char *formatter) { \
+    list_node *temp = self->head; \
+    while (temp != NULL) { \
+        printf(formatter, temp->value); \
+        temp = temp->next; \
+    } \
+} \
+ \
+list initList() { \
+ \
+    list list_default = { \
+        &list_createNode, \
+        &list_print, \
+        0 \
+    }; \
+    return list_default; \
+}
+
+/*
+--------------------------------------------------
     Better Strings
 --------------------------------------------------
 */
@@ -136,7 +188,6 @@ string initString(char *initstring) {
         immutablestring[i] = initstring[i];
     } immutablestring[stringlen] = '\0';
     
-
     string string_default = {
         immutablestring,
         &string_print,
--- simplewebrequests.h
+++ simplewebrequests.h
@@ -3,7 +3,6 @@
 
 #ifdef _WIN32
 #include <WinSock2.h>
-#include <WS2tcpip.h>
 #endif
 
 #ifdef linux
@@ -30,47 +29,55 @@ typedef struct http_s {
     struct website_s website;
     void (*get)(struct http_s *self, char *url);
     void (*post)(struct http_s *self, char *url, char *postfields);
-} http;
+} http_t;
 
-void http_get(http *self, char *url) {
-
-
+void http_get(http_t *self, char *url) {
 
 #ifdef _WIN32
-    
     WSADATA wsaData;
     SOCKET sock;
     struct hostent *host;
-    
-    
+    struct in_addr **addr_list;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("ERROR: WSAStartup failed to run.\n");
         return;
     }
+#endif
+#ifdef linux
+    int sock, errornum;
+#endif
 
-    struct addrinfo hints;
+    struct addrinfo hints, *servinfo;
     
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) != 0) {
         printf("ERROR: Failed to create socket.\n");
         return;
     }
 
-    host = gethostbyname(url);
+    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
+    hints.ai_addr->sa_data = 
 
-    getaddrinfo()
+    #ifdef linux
+    if ((errornum = getaddrinfo(url, "80", &hints, &servinfo)) != 0) {
+        printf("ERROR: Failed to getaddrinfo with error %s\n", gai_strerror(errornum));
+        return;
+    }
+    #endif
 
-
-    if (connect(sock, (struct sockaddr *)(&hints), sizeof(hints)) != 0) {
-        printf("ERROR: Failed to connect.\n");
+    if ((host = gethostbyname(url)) == NULL) {
+        printf("ERROR: Failed to get host by name.\n");
         return;
     }
 
-    printf("success");
+    //if (connect(sock, (struct sockaddr *)(&hints), sizeof(hints)) != 0) {
+    //    printf("ERROR: Failed to connect.\n");
+    //    return;
+    //}
+
+    
+    printf("IP address: %s\n", inet_ntoa();
     closesocket(sock);
     WSACleanup();
-
-#endif
-
 }
